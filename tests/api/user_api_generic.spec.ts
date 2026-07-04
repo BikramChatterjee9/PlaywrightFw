bfff568c9f5853c28b882bb7f5d4aa1cea7a5e65
BikramChatterjee9 <34390859+BikramChatterjee9@users.noreply.github.com>
2026-07-04 16:28:25 +0530

    new yml file

--- tests/api/user_api_generic.spec.ts
+++ tests/api/user_api_generic.spec.ts
@@ -1,55 +1,55 @@
-import { test, expect } from '../../src/fixtures/apiFixture'
+// import { test, expect } from '../../src/fixtures/apiFixture'
 
-let TOKEN = process.env.API_TOKEN
-let AUTH_HEADER = { Authorization: `Bearer ${TOKEN}` }
+// let TOKEN = process.env.API_TOKEN
+// let AUTH_HEADER = { Authorization: `Bearer ${TOKEN}` }
 
 
-async function createUser(apiHelper: any) {
-    let userObj = {
-        "name": "Jagmeet Guneta",
-        "email": `automation_${Date.now()}@open.com`,
-        "gender": "female",
-        "status": "active"
-    }
-    let response = await apiHelper.post('/public/v2/users', userObj, AUTH_HEADER)
-    return response.body
-}
+// async function createUser(apiHelper: any) {
+//     let userObj = {
+//         "name": "Jagmeet Guneta",
+//         "email": `automation_${Date.now()}@open.com`,
+//         "gender": "female",
+//         "status": "active"
+//     }
+//     let response = await apiHelper.post('/public/v2/users', userObj, AUTH_HEADER)
+//     return response.body
+// }
 
-test('post and get call', async ({ apiHelper }) => {
-    let response = await createUser(apiHelper)
-    let userId = await response.id
-    console.log(userId)
+// test('post and get call', async ({ apiHelper }) => {
+//     let response = await createUser(apiHelper)
+//     let userId = await response.id
+//     console.log(userId)
 
-    let responseGet = await apiHelper.get(`/public/v2/users/${userId}`, AUTH_HEADER)
-    expect(userId).toBe(responseGet.body.id)
-})
+//     let responseGet = await apiHelper.get(`/public/v2/users/${userId}`, AUTH_HEADER)
+//     expect(userId).toBe(responseGet.body.id)
+// })
 
-test('post and put call', async ({ apiHelper }) => {
-    let response = await createUser(apiHelper)
-    let userId = await response.id
-    console.log(userId)
+// test('post and put call', async ({ apiHelper }) => {
+//     let response = await createUser(apiHelper)
+//     let userId = await response.id
+//     console.log(userId)
 
-    let updateUser = {
-        "name": "Jagmeet New",
-        "email": `automation_${Date.now()}@open.com`,
-        "gender": "female",
-        "status": "inactive"
-    }
+//     let updateUser = {
+//         "name": "Jagmeet New",
+//         "email": `automation_${Date.now()}@open.com`,
+//         "gender": "female",
+//         "status": "inactive"
+//     }
 
-    let responseGet = await apiHelper.put(`/public/v2/users/${userId}`, updateUser, AUTH_HEADER)
-    expect(userId).toBe(responseGet.body.id)
-    expect(updateUser.name).toBe(responseGet.body.name)
-})
+//     let responseGet = await apiHelper.put(`/public/v2/users/${userId}`, updateUser, AUTH_HEADER)
+//     expect(userId).toBe(responseGet.body.id)
+//     expect(updateUser.name).toBe(responseGet.body.name)
+// })
 
-test('post and delete call', async ({ apiHelper }) => {
-    let response = await createUser(apiHelper)
-    let userId = await response.id
-    console.log(userId)
+// test('post and delete call', async ({ apiHelper }) => {
+//     let response = await createUser(apiHelper)
+//     let userId = await response.id
+//     console.log(userId)
 
-    let responseGet = await apiHelper.delete(`/public/v2/users/${userId}`, AUTH_HEADER)
-    expect(responseGet.status).toBe(200)
+//     let responseGet = await apiHelper.delete(`/public/v2/users/${userId}`, AUTH_HEADER)
+//     expect(responseGet.status).toBe(200)
 
-    let responseGet1 = await apiHelper.get(`/public/v2/users/${userId}`, AUTH_HEADER)
+//     let responseGet1 = await apiHelper.get(`/public/v2/users/${userId}`, AUTH_HEADER)
     
 
-})
+// })
--- tests/api/users_api.spec.ts
+++ tests/api/users_api.spec.ts
@@ -1,19 +1,19 @@
-import { test, expect } from '../../src/fixtures/apiFixture'
+// import { test, expect } from '../../src/fixtures/apiFixture'
 
-let TOKEN = process.env.API_TOKEN
-let AUTH_HEADER = { Authorization: `Bearer ${TOKEN}` }
+// let TOKEN = process.env.API_TOKEN
+// let AUTH_HEADER = { Authorization: `Bearer ${TOKEN}` }
 
-async function createUser(apiHelper:any) {
-    let userObj = {
-        "name": "Jagmeet Guneta",
-        "email": `automation_${Date.now()}@open.com`,
-        "gender": "female",
-        "status": "active"
-    }
-    let response = await apiHelper.post('/public/v2/users', userObj, AUTH_HEADER)
-    expect(response.status).toBe(201)
-    return response.body
-}
+// async function createUser(apiHelper:any) {
+//     let userObj = {
+//         "name": "Jagmeet Guneta",
+//         "email": `automation_${Date.now()}@open.com`,
+//         "gender": "female",
+//         "status": "active"
+//     }
+//     let response = await apiHelper.post('/public/v2/users', userObj, AUTH_HEADER)
+//     expect(response.status).toBe(201)
+//     return response.body
+// }
 
 // test('create user test',async({apiHelper})=>{
 
